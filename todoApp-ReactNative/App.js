67ea927acce140985dabf77781dd0fd0b575e466
downhillskater91 <varuns.nandyal@gmail.com>
2020-10-14 21:06:32 -0500

    Copied ReactJS code & converted to React Native

--- todoApp-ReactNative/App.js
+++ todoApp-ReactNative/App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View, Button, Text, ScrollView} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+let id = 0;
+
+const Todo = (props) => (
+  <View>
+    <Button onPress={props.onDelete} title="delete"/>
+    <Text>{props.todo.text}</Text>
+  </View>
+);
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+
+  addTodo() {
+    id++;
+    const text = `TODO number ${id}`;
+    this.setState({
+      todos: [...this.state.todos, { id: id, text: text, checked: false }]
+    });
+  }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter((todo) => todo.id !== id)
+    });
+  }
+
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id !== id) return todo;
+        return {
+          id: todo.id,
+          text: todo.text,
+          checked: !todo.checked
+        };
+      })
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Todo count: {this.state.todos.length}</Text>
+        <Text>
+          Unchecked todo count:{" "}
+          {this.state.todos.filter((todo) => !todo.checked).length}
+        </Text>
+        <Button onPress={() => this.addTodo()} title="Add TODO" />
+        <ScrollView>
+          {this.state.todos.map((todo) => (
+            <Todo
+              onToggle={() => this.toggleTodo(todo.id)}
+              onDelete={() => this.removeTodo(todo.id)}
+              todo={todo}
+            />
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
